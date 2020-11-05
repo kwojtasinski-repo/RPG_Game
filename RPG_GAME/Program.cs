4087f19a0376119a2c33d17326aceed319aa8fc4
KW <KW*KW>
2020-11-05 13:54:57 +0100

    First commit with refactor app

---
+++ RPG_GAME/Program.cs
@@ -0,0 +1,17 @@
+﻿using RPG_GAME.Service.Concrete;
+using RPG_GAME.Service.Managers;
+using System;
+using RPG_GAME.Helpers;
+using RPG_GAME.Core.Entity;
+
+namespace RPG_GAME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // project should have at least 2 lib projects
+        }
+
+    }
+}
