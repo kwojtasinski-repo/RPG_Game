a84533bbdce92a7361b731627925612bad21d3fa
KW <k.wojtasinski@wp.pl>
2022-07-31 18:28:04 +0200

    RPG_GAME - API
    Added API and example endpoint

---
+++ src/RPG_GAME.Api/Controllers/AccountsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPG_GAME.Application.DTO;
+using RPG_GAME.Application.Services;
+
+namespace RPG_GAME.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IIdentityService _identityService;
+
+        public AccountsController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(SignUpDto signUpDto)
+        {
+            await _identityService.SignUpAsync(signUpDto);
+            return Ok();
+        }
+    }
+}
