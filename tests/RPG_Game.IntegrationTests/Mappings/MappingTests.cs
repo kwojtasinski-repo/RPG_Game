48b218fb15346ed0e6a781be41746749b28a5efd
KW <k.wojtasinski@wp.pl>
2022-07-31 12:49:59 +0200

    RPG GAME - Integration Tests
    Added tests for User and Email mappings

--- tests/RPG_Game.IntegrationTests/Mappings/MappingTests.cs
+++ tests/RPG_Game.IntegrationTests/Mappings/MappingTests.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Xunit;
 using System;
 using System.Collections.Generic;
+using RPG_GAME.Core.ValueObjects;
+using MongoDB.Bson.Serialization;
+using System.Linq;
 
 namespace RPG_Game.IntegrationTests
 {
@@ -38,6 +41,20 @@ namespace RPG_Game.IntegrationTests
             var mapFromDb = await collection.Find(m => m.Id == map.Id).SingleOrDefaultAsync();
             mapFromDb.ShouldNotBeNull();
             mapFromDb.Name.ShouldBe(map.Name);
+        }
+
+        [Fact]
+        public async Task should_add_user_to_database()
+        {
+            var database = _mongoClient.GetDatabase(DatabaseName);
+            var collection = database.GetCollection<User>("users");
+            var user = new User { Id = Guid.NewGuid(), Email = Email.From("test@test.com"), Password = "test" };
+
+            await collection.InsertOneAsync(user);
+
+            var mapFromDb = await collection.Find(m => m.Id == user.Id).SingleOrDefaultAsync();
+            mapFromDb.ShouldNotBeNull();
+            mapFromDb.Email.ShouldBe(user.Email);
         }
 
         private const string ConnectionString = "mongodb://localhost:27017";
