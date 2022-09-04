1e38fc436aa6e64051801fe314a6043ef109e37f
KW <k.wojtasinski@wp.pl>
2022-09-04 23:33:00 +0200

    RPG_GAME - Infrastructure
    Added reading metada on client side

--- tests/RPG_GAME.IntegrationTests/Grpc/BattleServiceTests.cs
+++ tests/RPG_GAME.IntegrationTests/Grpc/BattleServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using Grpc.Core;
+using Humanizer;
 using RPG_GAME.Application.Exceptions.Auth;
 using RPG_GAME.IntegrationTests.Common;
 using RPG_GAME.IntegrationTests.Common.Protos;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -23,14 +25,20 @@ namespace RPG_GAME.IntegrationTests.Grpc
             var userId = Guid.NewGuid();
             var exceptionThrown = new UserNotFoundException(userId);
             var expectedException = new RpcException(new Status(StatusCode.FailedPrecondition, exceptionThrown.Message));
+            var task = client.PrepareBattleAsync(new PrepareBattleRequest { MapId = Guid.NewGuid().ToString(), UserId = userId.ToString() });
 
             // Act
-            var exception = await Record.ExceptionAsync(() => client.PrepareBattleAsync(new PrepareBattleRequest { MapId = Guid.NewGuid().ToString(), UserId = userId.ToString() }).ResponseAsync);
+            var exception = await Record.ExceptionAsync(() => task.ResponseAsync);
+            var headers = await task.ResponseHeadersAsync;
 
             // Assert
             Assert.NotNull(exception);
             Assert.IsType(expectedException.GetType(), exception);
             Assert.Equal(expectedException.Message, exception.Message);
+            Assert.NotNull(headers);
+            Assert.NotEmpty(headers);
+            Assert.Contains(headers.First().Key, typeof(UserNotFoundException).Name.Underscore());
+            Assert.Contains(headers.First().Value, expectedException.Message);
         }
     }
 }
