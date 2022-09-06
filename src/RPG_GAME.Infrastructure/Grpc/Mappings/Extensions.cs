ef3f7a0da663145cd7c46dcfa77b88f359b45c10
KW <k.wojtasinski@wp.pl>
2022-09-06 18:04:14 +0200

    RPG_GAME - Infrastructure
    Addes mappings method

---
+++ src/RPG_GAME.Infrastructure/Grpc/Mappings/Extensions.cs
@@ -0,0 +1,116 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using RPG_GAME.Application.DTO.Battles;
+using RPG_GAME.Infrastructure.Grpc.Protos;
+using System.Globalization;
+
+namespace RPG_GAME.Infrastructure.Grpc.Mappings
+{
+    internal static class Extensions
+    {
+        public static BattleResponse AsResponse(this BattleDetailsDto battleDetailsDto)
+        {
+            var response = new BattleResponse() { Id = battleDetailsDto.Id.ToString(), UserId = battleDetailsDto.UserId.ToString(), BattleInfo = battleDetailsDto.BattleInfo, StartDate = Timestamp.FromDateTime(battleDetailsDto.StartDate), EndDate = Timestamp.FromDateTime(battleDetailsDto.EndDate.Value) };
+            response.Map = new Map { Id = battleDetailsDto.Map.Id.ToString(), Difficulty = battleDetailsDto.Map.Difficulty, Name = battleDetailsDto.Map.Name };
+            foreach (var enemies in battleDetailsDto.Map.Enemies)
+            {
+                var enemy = new Enemies
+                {
+                    Enemy = new EnemyAssign
+                    {
+                        Id = enemies.Enemy.Id.ToString(),
+                        Name = enemies.Enemy.EnemyName,
+                        Attack = enemies.Enemy.BaseAttack,
+                        Category = enemies.Enemy.Category,
+                        Difficulty = enemies.Enemy.Difficulty,
+                        HealLvl = enemies.Enemy.BaseHealLvl,
+                        Health = enemies.Enemy.BaseHealth,
+                        Experience = new DecimalValue { Units = long.Parse(enemies.Enemy.Experience.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(enemies.Enemy.Experience.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
+                    },
+                    Quantity = enemies.Quantity
+                };
+
+                foreach (var skill in enemies.Enemy.Skills)
+                {
+                    enemy.Enemy.Skills.Add(new SkillEnemyAssign
+                    {
+                        Id = skill.Id.ToString(),
+                        Name = skill.Name,
+                        Attack = skill.BaseAttack,
+                        Probability = new DecimalValue { Units = long.Parse(skill.Probability.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(skill.Probability.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) }
+                    });
+                }
+
+                response.Map.Enemies.Add(enemy);
+            }
+
+            foreach (var enemyKilled in battleDetailsDto.EnemiesKilled)
+            {
+                var dict = new Dictionary();
+                dict.Pairs.Add(new Pair { Key = enemyKilled.Key.ToString(), Value = enemyKilled.Value });
+                response.EnemiesKilled.Add(dict);
+            }
+
+            foreach (var battle in battleDetailsDto.BattleStates)
+            {
+                var battleState = new BattleState
+                {
+                    Id = battle.Id.ToString(),
+                    BattleId = battle.BattleId.ToString(),
+                    BattleStatus = battle.BattleStatus,
+                    Created = Timestamp.FromDateTime(battle.Created),
+                    Player = new Player
+                    {
+                        Id = battle.Player.Id.ToString(),
+                        Name = battle.Player.Name,
+                        UserId = battle.Player.UserId.ToString(),
+                        Level = battle.Player.Level,
+                        CurrentExp = new DecimalValue { Units = long.Parse(battle.Player.CurrentExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(battle.Player.CurrentExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
+                        RequiredExp = new DecimalValue { Units = long.Parse(battle.Player.RequiredExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(battle.Player.RequiredExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
+                        Hero = new HeroAssign
+                        {
+                            Id = battle.Player.Hero.Id.ToString(),
+                            HeroName = battle.Player.Hero.HeroName,
+                            Attack = battle.Player.Hero.Attack,
+                            Health = battle.Player.Hero.Health
+                        }
+                    }
+                };
+
+                foreach (var skill in battle.Player.Hero.Skills)
+                {
+                    battleState.Player.Hero.Skills.Add(new SkillHeroAssign { Id = skill.Id.ToString(), Name = skill.Name, Attack = skill.Attack });
+                }
+
+                if (battle.Modified.HasValue)
+                {
+                    battleState.Modified = Timestamp.FromDateTime(battle.Modified.Value);
+                }
+
+                response.BattleStates.Add(battleState);
+            }
+
+            return response;
+        }
+
+        public static AddBattleEventResponse AsResponse(this BattleEventDto battleEventDto)
+        {
+            var response = new AddBattleEventResponse
+            {
+                Id = battleEventDto.Id.ToString(),
+                Action = new FightAction
+                {
+                    CharacterId = battleEventDto.Action.CharacterId.ToString(),
+                    Name = battleEventDto.Action.Name,
+                    Character = battleEventDto.Action.Character,
+                    AttackInfo = battleEventDto.Action.AttackInfo,
+                    DamageDealt = battleEventDto.Action.DamageDealt,
+                    Health = battleEventDto.Action.Health
+                },
+                BattleId = battleEventDto.BattleId.ToString(),
+                Created = Timestamp.FromDateTime(battleEventDto.Created),
+            };
+
+            return response;
+        }
+    }
+}
--- src/RPG_GAME.Infrastructure/Grpc/Services/BattleService.cs
+++ src/RPG_GAME.Infrastructure/Grpc/Services/BattleService.cs
@@ -4,6 +4,7 @@ using RPG_GAME.Application.Commands.Battles;
 using RPG_GAME.Infrastructure.Commands;
 using RPG_GAME.Infrastructure.Grpc.Protos;
 using System.Globalization;
+using RPG_GAME.Infrastructure.Grpc.Mappings;
 
 namespace RPG_GAME.Infrastructure.Grpc.Services
 {
@@ -23,81 +24,7 @@ namespace RPG_GAME.Infrastructure.Grpc.Services
             var mapId = Guid.Parse(request.MapId);
             var userId = Guid.Parse(request.UserId);
             var battleDetails = await _commandDispatcher.SendAsync(new PrepareBattle { MapId = mapId, UserId = userId });
-            var response = new BattleResponse() { Id = battleDetails.Id.ToString(), UserId = battleDetails.UserId.ToString(), BattleInfo = battleDetails.BattleInfo, StartDate = Timestamp.FromDateTime(battleDetails.StartDate) };
-            response.Map = new Map { Id = battleDetails.Map.Id.ToString(), Difficulty = battleDetails.Map.Difficulty, Name = battleDetails.Map.Name };
-            foreach(var enemies in battleDetails.Map.Enemies)
-            {
-                var enemy = new Enemies
-                {
-                    Enemy = new EnemyAssign
-                    {
-                        Id = enemies.Enemy.Id.ToString(),
-                        Name = enemies.Enemy.EnemyName,
-                        Attack = enemies.Enemy.BaseAttack,
-                        Category = enemies.Enemy.Category,
-                        Difficulty = enemies.Enemy.Difficulty,
-                        HealLvl = enemies.Enemy.BaseHealLvl,
-                        Health = enemies.Enemy.BaseHealth,
-                        Experience = new DecimalValue { Units = long.Parse(enemies.Enemy.Experience.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(enemies.Enemy.Experience.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
-                    },
-                    Quantity = enemies.Quantity
-                };
-
-                foreach (var skill in enemies.Enemy.Skills)
-                {
-                    enemy.Enemy.Skills.Add(new SkillEnemyAssign { Id = skill.Id.ToString(), Name = skill.Name, Attack = skill.BaseAttack, 
-                        Probability = new DecimalValue { Units = long.Parse(skill.Probability.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(skill.Probability.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) }});
-                }
-
-                response.Map.Enemies.Add(enemy);
-            }
-
-            foreach (var enemyKilled in battleDetails.EnemiesKilled)
-            {
-                var dict = new Dictionary();
-                dict.Pairs.Add(new Pair { Key = enemyKilled.Key.ToString(), Value = enemyKilled.Value });
-                response.EnemiesKilled.Add(dict);
-            }
-
-            foreach (var battle in battleDetails.BattleStates)
-            {
-                var battleState = new BattleState
-                {
-                    Id = battle.Id.ToString(),
-                    BattleId = battle.BattleId.ToString(),
-                    BattleStatus = battle.BattleStatus,
-                    Created = Timestamp.FromDateTime(battle.Created),
-                    Player = new Player
-                    {
-                        Id = battle.Player.Id.ToString(),
-                        Name = battle.Player.Name,
-                        UserId = battle.Player.UserId.ToString(),
-                        Level = battle.Player.Level,
-                        CurrentExp = new DecimalValue { Units = long.Parse(battle.Player.CurrentExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(battle.Player.CurrentExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
-                        RequiredExp = new DecimalValue { Units = long.Parse(battle.Player.RequiredExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(battle.Player.RequiredExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
-                        Hero = new HeroAssign
-                        {
-                            Id = battle.Player.Hero.Id.ToString(),
-                            HeroName = battle.Player.Hero.HeroName,
-                            Attack = battle.Player.Hero.Attack,
-                            Health = battle.Player.Hero.Health
-                        }
-                    }
-                };
-
-                foreach (var skill in battle.Player.Hero.Skills)
-                {
-                    battleState.Player.Hero.Skills.Add(new SkillHeroAssign { Id = skill.Id.ToString(), Name = skill.Name, Attack = skill.Attack });
-                }
-
-                if (battle.Modified.HasValue)
-                {
-                    battleState.Modified = Timestamp.FromDateTime(battle.Modified.Value);
-                }
-
-                response.BattleStates.Add(battleState);
-            }
-
+            var response = battleDetails.AsResponse();
             return response;
         }
 
@@ -115,86 +42,7 @@ namespace RPG_GAME.Infrastructure.Grpc.Services
             var battleId = Guid.Parse(request.BattleId);
             var userId = Guid.Parse(request.UserId);
             var battleDetails = await _commandDispatcher.SendAsync(new CompleteBattle { BattleId = battleId, UserId = userId });
-            var response = new BattleResponse() { Id = battleDetails.Id.ToString(), UserId = battleDetails.UserId.ToString(), BattleInfo = battleDetails.BattleInfo, StartDate = Timestamp.FromDateTime(battleDetails.StartDate), EndDate = Timestamp.FromDateTime(battleDetails.EndDate.Value) };
-            response.Map = new Map { Id = battleDetails.Map.Id.ToString(), Difficulty = battleDetails.Map.Difficulty, Name = battleDetails.Map.Name };
-            foreach (var enemies in battleDetails.Map.Enemies)
-            {
-                var enemy = new Enemies
-                {
-                    Enemy = new EnemyAssign
-                    {
-                        Id = enemies.Enemy.Id.ToString(),
-                        Name = enemies.Enemy.EnemyName,
-                        Attack = enemies.Enemy.BaseAttack,
-                        Category = enemies.Enemy.Category,
-                        Difficulty = enemies.Enemy.Difficulty,
-                        HealLvl = enemies.Enemy.BaseHealLvl,
-                        Health = enemies.Enemy.BaseHealth,
-                        Experience = new DecimalValue { Units = long.Parse(enemies.Enemy.Experience.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(enemies.Enemy.Experience.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
-                    },
-                    Quantity = enemies.Quantity
-                };
-
-                foreach (var skill in enemies.Enemy.Skills)
-                {
-                    enemy.Enemy.Skills.Add(new SkillEnemyAssign
-                    {
-                        Id = skill.Id.ToString(),
-                        Name = skill.Name,
-                        Attack = skill.BaseAttack,
-                        Probability = new DecimalValue { Units = long.Parse(skill.Probability.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(skill.Probability.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) }
-                    });
-                }
-
-                response.Map.Enemies.Add(enemy);
-            }
-
-            foreach (var enemyKilled in battleDetails.EnemiesKilled)
-            {
-                var dict = new Dictionary();
-                dict.Pairs.Add(new Pair { Key = enemyKilled.Key.ToString(), Value = enemyKilled.Value });
-                response.EnemiesKilled.Add(dict);
-            }
-
-            foreach (var battle in battleDetails.BattleStates)
-            {
-                var battleState = new BattleState
-                {
-                    Id = battle.Id.ToString(),
-                    BattleId = battle.BattleId.ToString(),
-                    BattleStatus = battle.BattleStatus,
-                    Created = Timestamp.FromDateTime(battle.Created),
-                    Player = new Player
-                    {
-                        Id = battle.Player.Id.ToString(),
-                        Name = battle.Player.Name,
-                        UserId = battle.Player.UserId.ToString(),
-                        Level = battle.Player.Level,
-                        CurrentExp = new DecimalValue { Units = long.Parse(battle.Player.CurrentExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(battle.Player.CurrentExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
-                        RequiredExp = new DecimalValue { Units = long.Parse(battle.Player.RequiredExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[0]), Nanos = int.Parse(battle.Player.RequiredExp.ToString("0.0000000", CultureInfo.InvariantCulture).Split('.')[1]) },
-                        Hero = new HeroAssign
-                        {
-                            Id = battle.Player.Hero.Id.ToString(),
-                            HeroName = battle.Player.Hero.HeroName,
-                            Attack = battle.Player.Hero.Attack,
-                            Health = battle.Player.Hero.Health
-                        }
-                    }
-                };
-
-                foreach (var skill in battle.Player.Hero.Skills)
-                {
-                    battleState.Player.Hero.Skills.Add(new SkillHeroAssign { Id = skill.Id.ToString(), Name = skill.Name, Attack = skill.Attack });
-                }
-
-                if (battle.Modified.HasValue)
-                {
-                    battleState.Modified = Timestamp.FromDateTime(battle.Modified.Value);
-                }
-
-                response.BattleStates.Add(battleState);
-            }
-
+            var response = battleDetails.AsResponse();
             return response;
         }
 
@@ -204,21 +52,7 @@ namespace RPG_GAME.Infrastructure.Grpc.Services
             var battleId = Guid.Parse(request.BattleId);
             var enemyId = Guid.Parse(request.EnemyId);
             var battleEvent = await _commandDispatcher.SendAsync(new AddBattleEvent { BattleId = battleId, PlayerId = playerId, EnemyId = enemyId, Action = request.Action });
-            var response = new AddBattleEventResponse
-            {
-                Id = battleEvent.Id.ToString(),
-                Action = new FightAction
-                {
-                    CharacterId = battleEvent.Action.CharacterId.ToString(),
-                    Name = battleEvent.Action.Name,
-                    Character = battleEvent.Action.Character,
-                    AttackInfo = battleEvent.Action.AttackInfo,
-                    DamageDealt = battleEvent.Action.DamageDealt,
-                    Health = battleEvent.Action.Health
-                },
-                BattleId = battleEvent.BattleId.ToString(),
-                Created = Timestamp.FromDateTime(battleEvent.Created),
-            };
+            var response = battleEvent.AsResponse();
             return response;
         }
     }
