e6235dc3856d6d08068ddd26a77519aa615d9479
KW <k.wojtasinski@wp.pl>
2022-11-13 19:20:29 +0100

    RPG_GAME - UI
    Added new enemies with actions depends on category

--- src/RPG_GAME.UI/src/services/EnemyService.js
+++ src/RPG_GAME.UI/src/services/EnemyService.js
@@ -1,4 +1,10 @@
 import enemyKnight from "@/assets/enemy-knight-reverse.png"
+import archerIdle from "@/assets/archer/idle-and-running.png"
+import archerAttack from "@/assets/archer/normal-attack.png"
+import archerDeath from "@/assets/archer/death.png"
+import dragonIdle from "@/assets/dragon/dragon-knight-idle.png"
+import dragonAttack from "@/assets/dragon/dragon-knight-attack.png"
+import dragonDeath from "@/assets/dragon/dragon-knight-death.png"
 
 export const actions = {
     idle: 1,
@@ -10,7 +16,7 @@ export const actions = {
 export default class EnemyService {
     constructor(category) {
         this.category = category;
-        this.enemy = new Knight();
+        this.enemy = selectEnemy(category);
         this.health = 100;
         this.currentHealth = 100;
         this.attack = {
@@ -46,7 +52,7 @@ export default class EnemyService {
 
 const assignBaseStats = (enemyTarget, category) => {
     enemyTarget.category = category;
-    enemyTarget.enemy = new Knight();
+    enemyTarget.enemy = selectEnemy(category);
     enemyTarget.health = 100;
     enemyTarget.currentHealth = 100;
     enemyTarget.attack = {
@@ -55,9 +61,115 @@ const assignBaseStats = (enemyTarget, category) => {
     }
 }
 
-class Knight {
+const selectEnemy = (category) => {
+    switch (category) {
+        case 'Archer': 
+            return new Archer();
+        case 'Knight':
+            return new Knight();
+        case 'Dragon':
+            return new Dragon();
+        default:
+            throw new Error(`There is no category enemy for ${category}`);
+    }
+}
+
+class Enemy {
+    update(deltaTime) {
+        if (this.frameTimer > this.frameInterval) {
+            this.frameTimer = 0;
+
+            if (this.frameX > this.minFrame) {
+                this.frameX--;
+            } else {
+                this.frameX = this.maxFrame;
+            }
+        } else {
+            this.frameTimer +=  deltaTime;
+        }
+    }
+}
+
+class Archer extends Enemy {
     constructor() {
+        super();
         this.frameX = 7;
+        this.maxFrame = 7;
+        this.frameY = 0;
+        this.image = new Image();
+        this.image.src = archerIdle;
+        this.fps = 15;
+        this.frameInterval = 1000 / this.fps;
+        this.frameTimer = 0;
+        this.width = 64;
+        this.height = 65;
+        this.x = 530;
+        this.y = 245;
+        this.offsetY = 0;
+        this.minFrame = 7;
+        this.currentAction = actions.idle;
+    }
+
+    draw(context) {
+        context.drawImage(this.image, this.frameX * this.width, (this.frameY * this.height) + this.offsetY, this.width, this.height, this.x, this.y, this.width * 2.25, this.height * 2.25);
+    }
+
+    selectAction(deltaTime) {
+        if (this.currentAction === actions.idle) {
+            this.offsetY = 0;
+            this.image.src = archerIdle;
+            this.minFrame = 7;
+            this.frameY = 0;
+            this.update(deltaTime);
+        } else if (this.currentAction === actions.fight) {
+            this.offsetY = 0;
+            this.image.src = archerAttack;
+            this.currentFrame = this.currentFrame ? this.currentFrame : 0;
+            const frames = [ { id: 0, x: 7, y: 0 }, { id: 1, x: 6, y: 0 }, { id: 2, x: 5, y: 0 }, { id: 3, x: 4, y: 0 }, { id: 4, x: 3, y: 0 }, { id: 5, x: 2, y: 0 }, { id: 6, x: 1, y: 0 }, 
+                { id: 7, x: 0, y: 0 }, { id: 8, x: 7, y: 1 }, { id: 9, x: 6, y: 1 }, { id: 10, x: 5, y: 1 } ];
+            
+            if (this.frameTimer > this.frameInterval) {
+                this.frameTimer = 0;
+    
+                if (this.currentFrame < 10) {
+                    this.frameX = frames[this.currentFrame].x;
+                    this.frameY = frames[this.currentFrame].y;
+                    this.currentFrame++;
+                } else {
+                    this.currentFrame = null;
+                    this.currentAction = actions.idle;
+                }
+            } else {
+                this.frameTimer +=  deltaTime;
+            }
+        } else if (this.currentAction === actions.deadAnimation) {
+            this.offsetY = 65;
+            this.image.src = archerDeath;
+            this.frameY = 0;
+            this.minFrame = 0;
+            this.update(deltaTime);
+            if (!this.actionInvoked) {
+                setTimeout(() => {
+                    this.currentAction = actions.dead;
+                    this.actionInvoked = false;
+                }, 500);
+                this.actionInvoked = true;
+            }
+        } else if (this.currentAction === actions.dead) {
+            this.offsetY = 65;
+            this.image.src = archerDeath;
+            this.frameY = 1;
+            this.minFrame = 7;
+            this.update(deltaTime);
+        }
+    }   
+}
+
+class Knight extends Enemy {
+    constructor() {
+        super();
+        this.frameX = 7;
+        this.maxFrame = 7;
         this.frameY = 0;
         this.image = new Image();
         this.image.src = enemyKnight;
@@ -72,20 +184,6 @@ class Knight {
         this.minFrame = 2;
         this.actionInvoked = false;
         this.currentAction = actions.idle;
-    }
-
-    update(deltaTime) {
-        if (this.frameTimer > this.frameInterval) {
-            this.frameTimer = 0;
-
-            if (this.frameX > this.minFrame) {
-                this.frameX--;
-            } else {
-                this.frameX = 7;
-            }
-        } else {
-            this.frameTimer +=  deltaTime;
-        }
     }
 
     draw(context) {
@@ -128,5 +226,95 @@ class Knight {
             this.minFrame = 7;
             this.update(deltaTime);
         }
-    }   
+    }
+}
+
+class Dragon extends Enemy {
+    constructor() {
+        super();
+        this.frameX = 0;
+        this.maxFrame = 3;
+        this.frameY = 0;
+        this.image = new Image();
+        this.image.src = dragonIdle;
+        this.fps = 10;
+        this.frameInterval = 1000 / this.fps;
+        this.frameTimer = 0;
+        this.width = 475;
+        this.height = 120;
+        this.x = 550;
+        this.y = 255;
+        this.offsetX = 250;
+        this.offsetY = 215;
+        this.minFrame = 0;
+        this.actionInvoked = false;
+        this.currentAction = actions.idle;
+    }
+    
+    draw(context) {
+        context.drawImage(this.image, (this.frameX * this.width) + this.offsetX, (this.frameY * this.height) + this.offsetY, this.width, this.height, this.x, this.y, this.width * 1, this.height * 1);
+    }
+
+    selectAction(deltaTime) {
+        if (this.currentAction === actions.idle) {
+            this.image.src = dragonIdle;
+            this.fps = 1;
+            this.width = 480;
+            this.height = 120;
+            this.offsetX = 250;
+            this.offsetY = 215;
+            this.minFrame = 0;
+            this.frameX = 0;
+            this.frameY = 0;
+        } else if (this.currentAction === actions.fight) {
+            this.minFrame = 0;
+            this.image.src = dragonAttack;
+            this.fps = 1;
+            this.width = 480;
+            this.height = 120;
+            this.offsetX = 150;
+            this.offsetY = 215;
+            this.minFrame = 0;
+            this.frameY = 0;
+            const nextFrame = this.frameX - 1;
+            this.update(deltaTime);
+            if (nextFrame < this.minFrame) {
+                if (!this.actionInvoked) {
+                    setTimeout(() => {
+                        this.currentAction = actions.idle;
+                        this.frameX = 0;
+                        this.actionInvoked = false;
+                    }, 500);
+                    this.actionInvoked = true;
+                }
+            }
+        } else if (this.currentAction === actions.deadAnimation) {
+            this.image.src = dragonAttack;dragonDeath
+            this.fps = 1;
+            this.width = 480;
+            this.height = 120;
+            this.offsetX = 150;
+            this.offsetY = 215;
+            this.minFrame = 0;
+            this.frameY = 0;
+            this.update(deltaTime);
+            if (!this.actionInvoked) {
+                setTimeout(() => {
+                    this.currentAction = actions.dead;
+                    this.actionInvoked = false;
+                }, 500);
+                this.actionInvoked = true;
+            }
+        } else if (this.currentAction === actions.dead) {
+            this.image.src = dragonDeath;
+            this.fps = 1;
+            this.width = 480;
+            this.height = 120;
+            this.offsetX = 150;
+            this.offsetY = 215;
+            this.minFrame = 0;
+            this.frameX = 0;
+            this.frameY = 0;
+        }
+    }
 }
